1e85eedaaa856b2cdd6b6e5f22590483ba609ce7
MichaszJ <mjski@live.ca>
2020-03-27 20:32:10 -0400

    Update P3-mechanism-sim.py

--- P3-mechanism-sim.py
+++ P3-mechanism-sim.py
@@ -13,31 +13,145 @@ chordLength = 1014.28
 quarterChord = chordLength/4
 quarterChordRemainder = chordLength - quarterChord
 
-def LEx(dx, theta):
+def QCx(dx, theta):
     return dx + quarterChord * math.cos(math.radians(theta))
 
-def LEy(dy, theta):
-    return dy + quarterChord * math.sin(math.radians(theta))
+def QCy(dy, theta):
+    return dy
 
-def TEx(dx, theta):
-    return chordLength + dx - quarterChordRemainder * math.cos(math.radians(theta))
+def LEx(QCx, theta):
+    return QCx - quarterChord * math.cos(math.radians(theta))
 
-def TEy(dy, theta):
-    return dy - quarterChordRemainder * math.sin(math.radians(theta))
+def LEy(QCy, theta):
+    return QCy + quarterChord * math.sin(math.radians(theta))
 
-fig, ax = plt.subplots(dpi = 260)
-for i in range(0, intervals):
-    ax.cla()
+def TEx(QCx, theta):
+    return QCx + quarterChordRemainder * math.cos(math.radians(theta))
 
-    dx = dxList[i]
-    dy = dyList[i]
-    theta = thetaList[i]
+def TEy(QCy, theta):
+    return QCy - quarterChordRemainder * math.sin(math.radians(theta))
 
-    ax.set(xlim = (-10, 2010), ylim=(-600, 100))
+def animate():
+    fig, ax = plt.subplots(dpi=260)
 
-    ax.plot([LEx(dx, theta), TEx(dx, theta)], [LEy(dy, theta), TEx(dy, theta)])
-    ax.plot([dx + quarterChord], [dy], marker='o')
+    for i in range(0, intervals):
+        ax.cla()
+        dx = dxList[i]
+        dy = dyList[i]
+        theta = thetaList[i]
 
-    ax.text(dx + quarterChord + 50, dy - 15, 'QC: {0} | {1}'.format(int(dx + quarterChord), int(dy)))
+        qcx = QCx(dx, theta)
+        qcy = QCy(dy, theta)
 
-    plt.pause(0.001)
+        lex = LEx(qcx, theta)
+        tex = TEx(qcx, theta)
+
+        ley = LEy(qcy, theta)
+        tey = TEy(qcy, theta)
+
+        ax.set(xlim = (-10, 1550), ylim=(-600, 300))
+
+        ax.plot([lex, tex], [ley, tey], marker='o')
+        ax.plot([qcx], [qcy], marker='o')
+
+        ax.text(qcx, qcy, 'QC: {0} | {1}'.format(int(dx + quarterChord), int(dy)))
+        ax.text(lex, ley, 'LE: {0} | {1}'.format(int(lex), int(ley)))
+        ax.text(tex, tey, 'TE: {0} | {1}'.format(int(tex), int(tey)))
+        ax.text(10, 250, r'$\theta$: {0}'.format(theta))
+
+        plt.pause(0.001)
+
+def halfDeployment():
+    fig, ax = plt.subplots(dpi=260)
+
+    interval = int(intervals/2)
+
+    dx = dxList[interval]
+    dy = dyList[interval]
+    theta = thetaList[interval]
+
+    qcx = QCx(dx, theta)
+    qcy = QCy(dy, theta)
+
+    lex = LEx(qcx, theta)
+    tex = TEx(qcx, theta)
+
+    ley = LEy(qcy, theta)
+    tey = TEy(qcy, theta)
+
+    ax.set(xlim = (-10, 1800), ylim=(-600, 300))
+
+    ax.plot([lex, tex], [ley, tey], marker='o')
+    ax.plot([qcx], [qcy], marker='o')
+
+    ax.text(qcx, qcy, 'QC: {0} | {1}'.format(int(dx + quarterChord), int(dy)))
+    ax.text(lex, ley, 'LE: {0} | {1}'.format(int(lex), int(ley)))
+    ax.text(tex, tey, 'TE: {0} | {1}'.format(int(tex), int(tey)))
+    ax.text(10, 250, r'$\theta$: {0}'.format(theta))
+
+    plt.show()
+
+def fullDeployment():
+    fig, ax = plt.subplots(dpi=260)
+
+    interval = intervals - 1
+
+    dx = dxList[interval]
+    dy = dyList[interval]
+    theta = thetaList[interval]
+
+    qcx = QCx(dx, theta)
+    qcy = QCy(dy, theta)
+
+    lex = LEx(qcx, theta)
+    tex = TEx(qcx, theta)
+
+    ley = LEy(qcy, theta)
+    tey = TEy(qcy, theta)
+
+    ax.set(xlim = (-10, 1800), ylim=(-600, 300))
+
+    ax.plot([lex, tex], [ley, tey], marker='o')
+    ax.plot([qcx], [qcy], marker='o')
+
+    ax.text(qcx, qcy, 'QC: {0} | {1}'.format(int(dx + quarterChord), int(dy)))
+    ax.text(lex, ley, 'LE: {0} | {1}'.format(int(lex), int(ley)))
+    ax.text(tex, tey, 'TE: {0} | {1}'.format(int(tex), int(tey)))
+    ax.text(10, 250, r'$\theta$: {0}'.format(theta))
+
+    plt.show()
+
+def stowed():
+    fig, ax = plt.subplots(dpi=260)
+
+    interval = 0
+
+    dx = dxList[interval]
+    dy = dyList[interval]
+    theta = thetaList[interval]
+
+    qcx = QCx(dx, theta)
+    qcy = QCy(dy, theta)
+
+    lex = LEx(qcx, theta)
+    tex = TEx(qcx, theta)
+
+    ley = LEy(qcy, theta)
+    tey = TEy(qcy, theta)
+
+    ax.set(xlim = (-10, 1800), ylim=(-600, 300))
+
+    ax.plot([lex, tex], [ley, tey], marker='o')
+    ax.plot([qcx], [qcy], marker='o')
+
+    ax.text(qcx, qcy, 'QC: {0} | {1}'.format(int(dx + quarterChord), int(dy)))
+    ax.text(lex, ley, 'LE: {0} | {1}'.format(int(lex), int(ley)))
+    ax.text(tex, tey, 'TE: {0} | {1}'.format(int(tex), int(tey)))
+    ax.text(10, 250, r'$\theta$: {0}'.format(theta))
+
+    plt.show()
+
+#stowed()
+#halfDeployment()
+#fullDeployment()
+animate()
